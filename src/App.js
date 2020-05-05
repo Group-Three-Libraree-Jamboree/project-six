ce20401d5b1f6334274316d32fae57defdf8ac3b
Nikita Guliaev <gulnikita63@gmail.com>
2020-05-05 19:51:47 -0400

    added calendar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
+import Calendar from './components/Calendar';
 import './App.scss';
-import axios from 'axios';
+// import axios from 'axios';
 import firebase from './components/firebase';
 
 class App extends Component {
-	constructor() {
-		super();
-	}
+	// constructor() {
+	// 	super();
+	// }
 
 	componentDidMount() {
 		const dbRef = firebase.database().ref();
 		console.log(dbRef);
 	}
 
+
+
 	render() {
 		return (
-			<div className="App">
-				<h1>Budget App</h1>
-			</div>
+		<div className='App'>
+			<h1>Budget App</h1>
+			{/* You need to install Calendar in iTerm: npm install react-datepicker --save */}
+			<Calendar />
+		</div>
 		);
 	}
 }
