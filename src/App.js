b2da7f71185155e190147411fdfeeddf91dc10ee
Nikita Guliaev <gulnikita63@gmail.com>
2020-05-08 14:22:44 -0400

    appending transaction WIP

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import firebase from './components/firebase';
 import Header from './components/Header';
 import Calendar from './components/Calendar';
-import Expenses from './components/Expenses';
+// import Expenses from './components/Expenses';
 import Footer from './components/Footer';
 import './App.scss';
 
@@ -17,7 +17,10 @@ class App extends Component {
 			total: 0,
 			afterSaving: 0,
 			dailyExpenses: 0,
-			calendarDate: '',
+			calendarDate: "",
+			transDescr: "",
+			transAmount: 0,
+			test: ["wefef", "wef"],
 		};
 	}
 
@@ -43,13 +46,15 @@ class App extends Component {
 
 	// grabs calender date from the calender component and adds it to state
 	getCalenderDate = (passedDate) => {
+		let copyDate = passedDate
 		this.setState({
-			calendarDate: passedDate,
+			calendarDate: copyDate,
 		});
 	};
 
 	componentDidMount() {
-		const dbRef = firebase.database().ref();
+		// const dbRef = firebase.database().ref();
+		
 	}
 
 	// this function grabs all input values from app.js and adds it to state
@@ -85,7 +90,67 @@ class App extends Component {
 		);
 	};
 
+	// handleChangeText = (event) =>{
+	// 	event.preventDefault();
+	// 	// console.log(event.target.value);
+	// 	this.setState({
+	// 		transDescr: event.target.value
+	// 	})
+	// 	console.log(`Number is:`, this.state.transDescr);
+	// }
+	handleChange = (event) =>{
+		event.preventDefault();
+		// console.log(event.target.value);
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+		// console.log(`Text is:`, this.state.transAmount);
+	}
+
+	addTransactionToDb = ()=>{
+		console.log(this.state.transDescr, this.state.transAmount);
+		const dbRef = firebase.database().ref();
+		const { transAmount, transDescr, calendarDate } = this.state;
+		let transactionsToStoreInFb = {
+			[calendarDate]: {
+				description: transDescr,
+				amount: transAmount,
+			},
+		};
+		dbRef.push(transactionsToStoreInFb);
+		// console.log(this.state.calendarDate);
+		this.reCalculateTotal();
+
+		// this.setState({
+		// 	test: [...this.state.tets, 'transDescr']
+		// })
+	}
+
+	reCalculateTotal = ()=>{
+		this.setState({
+			total: this.state.total - this.state.transAmount
+		}, ()=>{
+			const dailybudget = this.state.total / this.state.days;
+			this.setState({
+				dailybudget: dailybudget
+			})
+		})
+	}
+
+	appendTransaction = ()=>{
+		return (
+			<ul>
+				{
+					this.state.test.map( (descr, index)=>{
+						return <li key={index}>{descr}</li>
+					})
+				}
+			</ul>
+		)
+	}
+
 	render() {
+	
 		return (
 			<div className="App">
 				<Header />
@@ -131,6 +196,39 @@ class App extends Component {
 					</p>
 				</div>
 				{/* <Expenses /> */}
+				<div>
+					{/* <input
+					type="text"
+					value={objectInput}
+					onChange={(event) => this.handleChange(event, i)}
+					/> */}
+
+					<input
+					type="text"
+						name="transDescr"
+					// value={objectInput}
+						onChange={this.handleChange}
+					/>
+					<input
+					type="number"
+						name="transAmount"
+					// value={objectInput}
+					onChange={this.handleChange}
+					/>
+
+					<button
+						type="button"
+						onClick={this.addTransactionToDb}>+Add
+					</button>
+
+					<button
+					type="button"
+					// onClick={this.removeClick}
+					>Remove
+					</button>
+
+					{this.appendTransaction()}
+				</div>
 				<Footer />
 			</div>
 		);
