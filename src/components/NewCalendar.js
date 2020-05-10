7bc3c69a91f05d9f22f66feb7ece17f61757e847
Dustin <dmkelly100@gmail.com>
2020-05-10 14:49:08 -0400

    calendar can track number of days between date and store it in state

--- src/components/NewCalendar.js
+++ src/components/NewCalendar.js
@@ -4,8 +4,11 @@ class NewCalendar extends Component {
 	constructor() {
 		super();
 		this.state = {
+			todayFull: new Date(),
 			today: '',
 			selectedDate: '',
+			fullSelected: new Date(),
+			days:0,
 		};
 	}
 
@@ -19,15 +22,29 @@ class NewCalendar extends Component {
 			('0' + today.getDate()).slice(-2);
 
 		this.setState({
+			todayFull: today,
 			today: date,
 		});
 	};
 
 	whenDateChanges = (e) => {
 		const pickedDate = e.target.value;
-		this.setState({
-			today: pickedDate,
-		});
+		const fullSelectedDate = new Date(e.target.value);
+		this.setState(
+			{
+				today: pickedDate,
+				fullSelected: fullSelectedDate,
+			},
+			() => {
+				
+					const d1 = this.state.fullSelected 
+					const d2 =  this.state.todayFull;
+					
+					this.setState({
+						days: parseInt((d1 - d2) / (24 * 3600 * 1000)),
+					})
+			}
+		);
 	};
 
 	componentDidMount() {
