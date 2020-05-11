dedb8e16b4fe85f2e197c70c5d8d7e42a948a97a
heatherca <h.c.andreadis@gmail.com>
2020-05-11 09:35:51 -0400

    added link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<footer>
-			<p>Made by <a href="http://www.heatherandreadis.com/">Heather</a>, <a href="https://nikitaguliaev.com">Nikita</a>, Tiffany, and <a href="https://www.dustinkelly.dev/">Dustin</a></p>
+			<p>Made by <a href="http://www.heatherandreadis.com/">Heather</a>, <a href="https://nikitaguliaev.com">Nikita</a>, <a href="https://tiffany-tubi.com">Tiffany</a>, and <a href="https://www.dustinkelly.dev/">Dustin</a></p>
 		</footer>
 	);
 };
