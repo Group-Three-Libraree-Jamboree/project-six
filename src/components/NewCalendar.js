0eff14f232a5fc87af2982d184d00c43114c0676
Dustin <dmkelly100@gmail.com>
2020-05-10 21:55:53 -0400

    removed extraneous code

--- src/components/NewCalendar.js
+++ src/components/NewCalendar.js
@@ -12,6 +12,7 @@ class NewCalendar extends Component {
 		};
 	}
 
+	// This creates the date format needed to use in the calendar's value and adds it to state. It also grabs the current full js date and puts it state
 	getCurrentDate = () => {
 		const today = new Date();
 		const date =
@@ -20,14 +21,14 @@ class NewCalendar extends Component {
 			('0' + (today.getMonth() + 1)).slice(-2) +
 			'-' +
 			('0' + today.getDate()).slice(-2);
-			
-		
+
 		this.setState({
 			todayFull: today,
 			today: date,
 		});
 	};
 
+	// When the user changes the date this function will add the calendar format value to state. It also grabs the selected full js date and puts it state. 
 	whenDateChanges = (e) => {
 		const pickedDate = e.target.value;
 		const fullSelectedDate = new Date(e.target.value);
@@ -40,17 +41,19 @@ class NewCalendar extends Component {
 				const d1 = this.state.fullSelected;
 				const d2 = this.state.todayFull;
 
-				this.setState({
-
-				days: parseInt((d1 - d2) / (24 * 3600 * 1000) + 1),
-			}, ()=> {
-					//making sure the next paycheck is not today
-					if (this.state.days === 0) {
-
-						return alert("Please select a date different from today")
+				// determines the remaining days from current date to selected date.
+				this.setState(
+					{
+						days: parseInt((d1 - d2) / (24 * 3600 * 1000) + 1),
+					},
+					() => {
+						//making sure the next paycheck is not today
+						if (this.state.days === 0) {
+							return alert('Please select a date different from today');
+						}
+						this.getDaysLeft();
 					}
-					this.getDaysLeft()
-				});
+				);
 			}
 		);
 	};
@@ -60,6 +63,7 @@ class NewCalendar extends Component {
 		this.getDaysLeft();
 	}
 
+	// used to pass remaining days to App.js
 	getDaysLeft = (day) => {
 		day = this.state.days;
 		this.props.getDaysLeft(day);
@@ -74,7 +78,7 @@ class NewCalendar extends Component {
 					type="date"
 					id="transactions"
 					name="transactions"
-					min = {this.state.today}
+					min={this.state.today}
 				/>
 			</div>
 		);
--- src/components/TransactionRecords.js
+++ src/components/TransactionRecords.js
@@ -3,106 +3,120 @@ import firebase from 'firebase';
 import Transaction from './RemoveTrans';
 
 class TransactionRecords extends Component {
-    constructor(){
-        super();
-        this.state = {
-            transDescr: "",
-            transAmount: "",
-            allTransactions: [],
-        }
-    }
-    //onChange grabs values from input fields (transDescr and transAmount) and saves to the state
-    handleChange = (event) => {
-        event.preventDefault();
-        this.setState({
-            [event.target.name]: event.target.value,
-        })
-    }
+	constructor() {
+		super();
+		this.state = {
+			transDescr: '',
+			transAmount: '',
+			allTransactions: [],
+		};
+	}
+	//onChange grabs values from input fields (transDescr and transAmount) and saves to the state
+	handleChange = (event) => {
+		event.preventDefault();
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
 
-    //creates a new firebase record as a child of 'Transactions': transDescr and transAmount
-    addTransactionToDb = () => {
-        const dbRef = firebase.database().ref('Transactions');
-        const { transAmount, transDescr } = this.state;
-        let transactionsToStoreInFb = {
-            description: transDescr,
-            amount: transAmount,
-        };
-        dbRef.push(transactionsToStoreInFb);
+	//creates a new firebase record as a child of 'Transactions': transDescr and transAmount
+	addTransactionToDb = () => {
+		const dbRef = firebase.database().ref('Transactions');
+		const { transAmount, transDescr } = this.state;
+		let transactionsToStoreInFb = {
+			description: transDescr,
+			amount: transAmount,
+		};
+		dbRef.push(transactionsToStoreInFb);
 
-        // empty input fields on Submit
-        this.setState({
-            transDescr: "",
-            transAmount: "",
-        })
-    }
+		// empty input fields on Submit
+		this.setState({
+			transDescr: '',
+			transAmount: '',
+		});
+	};
 
+	//when the page is rendered, retrieves all "Transactions" records from firebase, creates a copy and saves to state (this.state.allTransactions)
+	componentDidMount() {
+		const dbRef = firebase.database().ref('Transactions');
+		dbRef.on('value', (result) => {
+			const data = result.val();
+			const allTransactions = [];
+			for (let key in data) {
+				allTransactions.push({
+					transName: data[key].description,
+					transAmount: data[key].amount,
+					transId: key,
+				});
+			}
+			this.setState({
+				allTransactions: allTransactions,
+			});
+		});
+	}
 
-    //when the page is rendered, retrieves all "Transactions" records from firebase, creates a copy and saves to state (this.state.allTransactions)
-    componentDidMount() {
-        const dbRef = firebase.database().ref("Transactions");
-        dbRef.on('value', (result) => {
-            const data = result.val();
-            const allTransactions = [];
-            for (let key in data) {
-                allTransactions.push({ transName: data[key].description, transAmount: data[key].amount, transId:key});
-            }
-            this.setState({
-                allTransactions: allTransactions,
-            });
-        });
-    }
+	//prints to the page all paired items (amount and description) from this.state.allTransactions
+	appendTransaction = () => {
+		return (
+			<ul className="transactions">
+				{this.state.allTransactions.map((trans, index) => {
+					return (
+						<Transaction
+							key={index}
+							description={trans.transName}
+							amount={trans.transAmount}
+							index={trans.transId}
+						/>
+					);
+				})}
+			</ul>
+		);
+	};
 
-    //prints to the page all paired items (amount and description) from this.state.allTransactions
-    appendTransaction = () => {
-        return (
-            <ul className="transactions">
-                {
-                    this.state.allTransactions.map((trans, index) => {
-                        return(
-                            <Transaction key={index} description={trans.transName} amount={trans.transAmount} index={trans.transId}/>
-                        )
-                    })
-                }
-            </ul>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<h3 className="addExpense">Add Expense</h3>
+				<div className="expenseInfo">
+					<label htmlFor="transDescr" className="sr-only">
+						Description of expense
+					</label>
+					<input
+						required
+						type="text"
+						name="transDescr"
+						id="transDescr"
+						onChange={this.handleChange}
+						placeholder="Expense description"
+						value={this.state.transDescr}
+					/>
+					<label htmlFor="transAmount" className="sr-only">
+						Description of expense
+					</label>
+					<input
+						required
+						type="number"
+						name="transAmount"
+						id="transAmount"
+						onChange={this.handleChange}
+						placeholder="Expense amount"
+						value={this.state.transAmount}
+					/>
+					<button
+						className="add"
+						type="button"
+						aria-label="add transaction"
+						onClick={this.addTransactionToDb}
+					>
+						+
+					</button>
+				</div>
 
-    render(){
-        return(
-            <div>
-            <h3 className="addExpense">Add Expense</h3>
-                <div className="expenseInfo">
-                    <label htmlFor="transDescr" className="sr-only">Description of expense</label>
-                    <input
-                        type="text"
-                        name="transDescr"
-                        id="transDescr"
-                        onChange={this.handleChange}
-                        placeholder= "Expense description"
-                        value={this.state.transDescr}
-                    />
-                    <label htmlFor="transAmount" className="sr-only">Description of expense</label>
-                    <input
-                        type="number"
-                        name="transAmount"
-                        id="transAmount"
-                        onChange={this.handleChange}
-                        placeholder= "Expense amount"
-                        value={this.state.transAmount}
-                    />
-                    <button
-                        className="add"
-                        type="button"
-                        aria-label="add transaction"
-                        onClick={this.addTransactionToDb}>+
-                    </button>
-                </div>
-
-                {/* appends all transaction records to the page */}
-                {this.appendTransaction()}
-            </div>
-        )
-    }
+				{/* appends all transaction records to the page */}
+				{this.appendTransaction()}
+			</div>
+		);
+	}
 }
 
 export default TransactionRecords;
